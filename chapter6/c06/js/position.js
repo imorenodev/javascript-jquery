926e68fb09bdf87db5b5df9cb5bcf57638911fc2
imorenodev <imorenodev@gmail.com>
2015-11-29 22:57:13 -0500

    Practice with cursor position

---
+++ chapter6/c06/js/position.js
@@ -0,0 +1,20 @@
+//grab elements to show position values
+var sx = document.getElementById('sx');
+var sy = document.getElementById('sy');
+var px = document.getElementById('px');
+var py = document.getElementById('py');
+var cx = document.getElementById('cx');
+var cy = document.getElementById('cy');
+
+//update elements to show current position of cursor
+function showPosition(event) {
+	sx.value = event.screenX;	
+	sy.value = event.screenY;
+	px.value = event.pageX;	
+	py.value = event.pageY;
+	cx.value = event.clientX;	
+	cy.value = event.clientY;
+}
+
+var el = document.getElementById('body');
+el.addEventListener('mousemove', showPosition, false);
